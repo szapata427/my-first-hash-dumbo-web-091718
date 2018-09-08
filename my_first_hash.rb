69b574acf62f42a3f0d95710858435abd652ca8c
Steven Zapata <szapata427@gmail.com>
2018-09-08 20:04:36 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,7 +34,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest << {"muskets" => 2, "gun powder" => 4}
+shipping_manifest += {"muskets" => 2, "gun powder" => 4}
 
 
 
